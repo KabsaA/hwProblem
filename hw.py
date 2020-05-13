a2754c7d7e664e9593140dffca8326a771c1ceb4
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2020-05-13 08:45:03 -0600

    solved

--- hw.py
+++ hw.py
@@ -5,13 +5,17 @@
 if it contains any duplicate numbers.'''
 
 
-lst = []
 
-def duplicates(nums):
+def duplicates(nums,x):
+    count = 0
     for i in nums:
-        for j in i:
-            if j == i:
-                j.append(i)
-    return lst
+        if (i == x):
+            count = count + 1
+    return str(x) + " occurs " + str(count) + " times."
 
-print(duplicates([1,2,2,3,4,5]))
+
+print(duplicates([1,2,2,3,4,5],2)) # 2
+print(duplicates([1,2,2,3,4,5,5,5],5)) # 3
+print(duplicates([1,2,2,3,4,5,5,5],0)) # 0
+#first commit didn't solve problem
+#there was a type error couldn't iterate over int
